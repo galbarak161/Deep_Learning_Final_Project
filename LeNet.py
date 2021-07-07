89d3b4e83857657a8a6f632aa3fdc9850894fc82
gal barak <galbarak161@gmail.com>
2021-07-07 09:09:51 +0300

    Added cfar-10 dataset

--- LeNet.py
+++ LeNet.py
@@ -3,8 +3,8 @@ from Model_Class import Model
 
 
 class LeNet(Model):
-    def __init__(self, use_spatial_transformer: bool):
-        self.model_name = 'LeNet'
+    def __init__(self, use_spatial_transformer: bool, dataset_name: str, num_of_classes: int):
+        self.model_name = 'LeNet' + '_' + dataset_name
         super().__init__(self.model_name, use_spatial_transformer)
 
         self.feature_extractor = nn.Sequential(
@@ -25,7 +25,7 @@ class LeNet(Model):
             nn.BatchNorm2d(84),
             nn.ReLU(),
             nn.Dropout(self.dropout_p),
-            nn.Conv2d(in_channels=84, out_channels=43, kernel_size=(1, 1))
+            nn.Conv2d(in_channels=84, out_channels=num_of_classes, kernel_size=(1, 1))
         )
 
         super().set_optimizer()
